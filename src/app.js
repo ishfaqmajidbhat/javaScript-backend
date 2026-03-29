8336529aa87633dc79d58ac8702a84af965fcda4
Ishfaq <ishfaq.pal@gmail.com>
2026-03-29 23:49:42 +0530

    all setup configured with basic api post test

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
-const app = express()
-app.use(cors({
+const app = express();
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
+// routes import
 
-export { app }
+import userRouter from "./routes/user.router.js";
+
+// routes decleration
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,16 +1,15 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next))
-        .catch((error) => next(error))
-    }
-}
-export { asyncHandler }
-
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+export { asyncHandler };
 
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
-
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
