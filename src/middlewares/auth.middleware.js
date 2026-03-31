d5c82fdbc412b2c55508b9342d878602432b0a35
Ishfaq <ishfaq.pal@gmail.com>
2026-03-31 23:48:36 +0530

    add auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("Authorization")?.replace("Brearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "invalid accessToken");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(410, error?.message || "invalid access Token");
+  }
+});
+export { verifyJWT };
